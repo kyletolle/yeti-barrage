44e1010e48b7ebd267a948627e51bd5dfc9cbaa3
Kyle Tolle <kyle@nullsix.com>
2012-09-08 12:31:10 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YetiBarrage::Application.config.secret_token = 'db34bc2d5e73803117e672be67a4b0d1e3dd479c3b4b777cf0aa2e8ecf62b84048cdd7d4a7e9552c733547d2feec51900d7c297a7f4431a4c55e300354f07e5a'
