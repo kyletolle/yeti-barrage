8087e85278d6be0c6486fff7ea376d7707480f11
Kyle Tolle <kyle@nullsix.com>
2012-09-08 21:57:12 -0600

    Map javascript moved to its own file

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,61 @@
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(38.830, -104.820),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  };
+  map = new google.maps.Map(document.getElementById("map_canvas"),
+    mapOptions);
+
+  yetis = {};
+  mapYetis();
+}
+
+function mapYetis() {
+  var yetiList = JSON.parse(getYetis());
+
+  for (i = 0; i < yetiList.length; i++) {
+    var yeti = yetiList[i];
+
+    var marker = createMarker(yeti);
+    marker.setMap(map);
+
+    var infoWindow = createInfoWindow(yeti);
+    attachInfoWindowToMarker(marker, infoWindow);
+  }
+}
+
+function getYetis() {
+  var xmlHttp = null;
+
+  xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", "/yetis.json", false);
+  xmlHttp.send(null);
+
+  return xmlHttp.responseText;
+}
+
+function createMarker(yeti) {
+  var yetiLatLong = new google.maps.LatLng(yeti.lat, yeti.long);
+
+  return new google.maps.Marker({
+    position: yetiLatLong,
+    title: yeti.name
+  });
+}
+
+function createInfoWindow(yeti) {
+  var text = "<p><b>Yeti</b>: " + yeti.name + "<br />" +
+    "<b>Latitude</b>: " + yeti.lat + "<br />" +
+    "<b>Longitude</b>: " + yeti.long + "</p>";
+
+  return new google.maps.InfoWindow({
+    content: text
+  });
+}
+
+function attachInfoWindowToMarker(marker, infoWindow) {
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.open(map, marker);
+  });
+}
