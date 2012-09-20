057ed2e2135c44faed85fdfab5b6c4216b28bd46
Kyle Tolle <kyle@nullsix.com>
2012-09-20 08:01:33 -0600

    HTML entities encoded
    Yeti name has HTML entities encoded before validation, so the encoded
    string must be less than 100 characters.
    Fixes #9.

--- app/models/yeti.rb
+++ app/models/yeti.rb
@@ -1,7 +1,15 @@
+require 'htmlentities' 
+
 class Yeti < ActiveRecord::Base
   attr_accessible :lat, :long, :name
 
   validates :name, { presence: true, length: { maximum: 100 } }
   validates :lat, { presence: true, numericality: { greater_than: -90, less_than: 90} }
   validates :long, { presence: true, numericality: { greater_than: -180, less_than: 180} }
+
+  # Encode entities before validation so that the encoded string is still required to
+  # be less than the maximum length.
+  before_validation do
+    self.name = HTMLEntities.new.encode(name)
+  end
 end
