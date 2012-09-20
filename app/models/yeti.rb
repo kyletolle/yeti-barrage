a029dbdb582860cab8a279f628a6b8bb18f0b7dd
Kyle Tolle <kyle@nullsix.com>
2012-09-20 07:58:14 -0600

    Yeti validation
    Yeti data is validated.
    Name must exist and be no more than 100 characters.
    Latitude must exist and be within the range -90 to 90.
    Longitude must exist and be within the range -180 to 180.
    Fixes #8.

--- app/models/yeti.rb
+++ app/models/yeti.rb
@@ -1,3 +1,7 @@
 class Yeti < ActiveRecord::Base
   attr_accessible :lat, :long, :name
+
+  validates :name, { presence: true, length: { maximum: 100 } }
+  validates :lat, { presence: true, numericality: { greater_than: -90, less_than: 90} }
+  validates :long, { presence: true, numericality: { greater_than: -180, less_than: 180} }
 end
